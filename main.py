5ea613f0f20846c51a3f1f45d7da77e9fdbd64f4
Macailh <salvadorenriquegb@gmail.com>
2023-07-03 20:57:41 -0600

    feat: convert img to grayscale

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import cv2
 
-
+# Read image
 image = cv2.imread("example.png")
 print(image)
 original_window_name = "Original Image"
 
+# Show original image in a window
 cv2.imshow(original_window_name, image)
+
+# Convert image to grayscale
+img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+invert_color = cv2.bitwise_not(img_gray)
